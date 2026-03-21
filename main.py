aaee3ed5ac0e040f280f73d8dc102471872c86b4
dahviddacoder <davidyyin@gmail.com>
2026-03-21 15:17:51 -0400

    wrote main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+
+triangle = str(input("Is your shape a right triangle? (yes/no)"))
+
+"yes" == True
+"no" == False
+
+if triangle == "yes":
+    print("Your shape is a right triangle.")
+    first_side = float(input("What is the length of the first side?"))
+    second_side = float(input("What is the length of the second side?"))
+    length_of_hypotenuse = (first_side**2 + second_side**2)**0.5
+    print("The length of the hypotenuse is", length_of_hypotenuse)
+
+else:
+    print("Your shape is not a right triangle.")
+    any_triangle = str(input("Is your shape any triangle? (yes/no)"))
+
+    if any_triangle == "yes":
+        print("Your shape is a triangle.")
+
+    else:
+        print("Your shape is not a triangle.")
